1a42db49f37b9c5586017cca5f65d5e6209c1b1e
Tony Tom K <tony.tomk@thermofisher.com>
2018-05-20 21:38:55 +0530

    tutorial examples
    angular workouts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,21 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+ serverElements=[{type:'server',name:'Test Server', content:'Just a test'}];
+
+ onServerAdded(serverData:{serverName: string,serverContent:string}) {
+  this.serverElements.push({
+    type: 'server',
+    name: serverData.serverName,
+    content: serverData.serverContent
+  });
+}
+
+onBlueprintAdded(blueprintData:{serverName: string,serverContent:string}) {
+   this.serverElements.push({
+   type: 'blueprint',
+   name: blueprintData.serverName,
+    content: blueprintData.serverContent
+  });
+}
 }
